f3d724b4da54d3645ce5d69acd1fd605c478de24
Dencho Tsanov <tsanov.dencho@gmail.com>
2019-05-23 09:26:43 +0300

    init user module

---
+++ models/enums/UserStatus.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace denchotsanov\user\models\enums;
+
+/**
+ * Class UserStatus
+ *
+ */
+class UserStatus
+{
+    const ACTIVE = 1;
+    const DELETED = 0;
+    /**
+     * @var string message category
+     */
+    public static $messageCategory = 'yii2module.user';
+    /**
+     * @var array
+     */
+    public static $list = [
+        self::ACTIVE => 'Active',
+        self::DELETED => 'Deleted',
+    ];
+}
