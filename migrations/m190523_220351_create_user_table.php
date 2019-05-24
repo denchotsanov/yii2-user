544caca21caf14b98186583af69127cf611a04eb
Dencho Tsanov <tsanov.dencho@gmail.com>
2019-05-24 12:12:35 +0300

    update migration

--- migrations/m190523_220351_create_user_table.php
+++ migrations/m190523_220351_create_user_table.php
@@ -13,15 +13,19 @@ class m190523_220351_create_user_table extends Migration
      */
     public function safeUp()
     {
+        if (Yii::$app->db->schema->getTableSchema('user') === null) {
+            $this->renameTable('{{%user}}', '{{%user_old}}');
+        }
         $this->createTable('{{%user}}', [
             'id' => $this->primaryKey(),
-            'email'=> $this->string()->unique()->notNull(),
-            'password_hash'=>$this->string()->notNull(),
-            'password_reset_token' =>$this->string()->unique()->notNull(),
+            'email' => $this->string()->unique()->notNull(),
+            'password_hash' => $this->string()->notNull(),
+            'password_reset_token' => $this->string()->unique()->notNull(),
             'auth_key' => $this->string()->notNull()->unique(),
-            'status' =>$this->integer(),
+            'status' => $this->integer(),
             'created_at' => $this->integer(),
             'updated_at' => $this->integer(),
+            'last_login' => $this->integer(),
             'deleted_at' => $this->integer()->defaultValue(0),
         ]);
     }
@@ -31,6 +35,9 @@ class m190523_220351_create_user_table extends Migration
      */
     public function safeDown()
     {
+        if (Yii::$app->db->schema->getTableSchema('user_old') === null) {
+            $this->renameTable('{{%user}}', '{{%user_old}}');
+        }
         $this->dropTable('{{%user}}');
     }
 }
