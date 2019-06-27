faac935e367849f14df02bed91f4f19be6c384e3
Dencho Tsanov <d.tsanov@atomsolutions.net>
2019-06-27 11:53:57 +0300

    add filter for access

---
+++ filters/AccessRule.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * User: dencho
+ */
+
+namespace denchotsanov\user\filters;
+
+use yii\filters\AccessRule as YiiAccessRule;
+
+class AccessRule extends YiiAccessRule
+{
+    /**
+     * @inheritdoc
+     */
+    protected function matchRole($user)
+    {
+        if (empty($this->roles)) {
+            return true;
+        }
+        foreach ($this->roles as $role) {
+            if ($role === '?') {
+                if (\Yii::$app->user->isGuest) {
+                    return true;
+                }
+            } elseif ($role === '@') {
+                if (!\Yii::$app->user->isGuest) {
+                    return true;
+                }
+            } elseif ($role === 'admin') {
+                if (!\Yii::$app->user->isGuest && \Yii::$app->user->identity->isAdmin) {
+                    return true;
+                }
+            } elseif ($user->can($role)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
