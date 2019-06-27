439be7e28ddb6b62e70e3ae121f68cd60ea29fe2
Dencho Tsanov <d.tsanov@atomsolutions.net>
2019-06-27 14:05:07 +0300

    update translation

---
+++ widgets/Connect.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace denchotsanov\user\widgets;
+
+use Yii;
+use yii\authclient\ClientInterface;
+use yii\authclient\widgets\AuthChoice;
+use yii\authclient\widgets\AuthChoiceAsset;
+use yii\helpers\Html;
+use yii\helpers\Url;
+
+class Connect extends AuthChoice
+{
+    /**
+     * @var array|null
+     */
+    public $accounts;
+    /**
+     * @inheritdoc
+     */
+    public $options = [];
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        AuthChoiceAsset::register(Yii::$app->view);
+        if ($this->popupMode) {
+            Yii::$app->view->registerJs("\$('#" . $this->getId() . "').authchoice();");
+        }
+        $this->options['id'] = $this->getId();
+        echo Html::beginTag('div', $this->options);
+    }
+    /**
+     * @inheritdoc
+     */
+    public function createClientUrl($provider)
+    {
+        if ($this->isConnected($provider)) {
+            return Url::to(['/user/settings/disconnect', 'id' => $this->accounts[$provider->getId()]->id]);
+        } else {
+            return parent::createClientUrl($provider);
+        }
+    }
+    /**
+     * @param ClientInterface $provider
+     * @return bool
+     */
+    public function isConnected(ClientInterface $provider)
+    {
+        return $this->accounts != null && isset($this->accounts[$provider->getId()]);
+    }
+}
