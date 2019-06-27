f1f578c18b7938f0b7564d377c56637a09d48896
Dencho Tsanov <d.tsanov@atomsolutions.net>
2019-06-27 11:54:46 +0300

    add setting controller

---
+++ controllers/SettingsController.php
@@ -0,0 +1,197 @@
+<?php
+namespace denchotsanov\user\controllers;
+
+use denchotsanov\user\Finder;
+use denchotsanov\user\models\Profile;
+use denchotsanov\user\models\SettingsForm;
+use denchotsanov\user\models\User;
+use denchotsanov\user\Module;
+use denchotsanov\user\traits\AjaxValidationTrait;
+use denchotsanov\user\traits\EventTrait;
+use Yii;
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+use yii\web\Controller;
+use yii\web\ForbiddenHttpException;
+use yii\web\NotFoundHttpException;
+use yii\web\Response;
+
+class SettingsController extends Controller
+{
+    use AjaxValidationTrait;
+    use EventTrait;
+    
+    const EVENT_BEFORE_PROFILE_UPDATE = 'beforeProfileUpdate';
+    const EVENT_AFTER_PROFILE_UPDATE = 'afterProfileUpdate';
+    const EVENT_BEFORE_ACCOUNT_UPDATE = 'beforeAccountUpdate';
+    const EVENT_AFTER_ACCOUNT_UPDATE = 'afterAccountUpdate';
+    const EVENT_BEFORE_CONFIRM = 'beforeConfirm';
+    const EVENT_AFTER_CONFIRM = 'afterConfirm';
+    const EVENT_BEFORE_DISCONNECT = 'beforeDisconnect';
+    const EVENT_AFTER_DISCONNECT = 'afterDisconnect';
+    const EVENT_BEFORE_DELETE = 'beforeDelete';
+    const EVENT_AFTER_DELETE = 'afterDelete';
+    /** @inheritdoc */
+    public $defaultAction = 'profile';
+    /** @var Finder */
+    protected $finder;
+    /**
+     * @param string           $id
+     * @param \yii\base\Module $module
+     * @param Finder           $finder
+     * @param array            $config
+     */
+    public function __construct($id, $module, Finder $finder, $config = [])
+    {
+        $this->finder = $finder;
+        parent::__construct($id, $module, $config);
+    }
+    /** @inheritdoc */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::class,
+                'actions' => [
+                    'disconnect' => ['post'],
+                    'delete'     => ['post'],
+                ],
+            ],
+            'access' => [
+                'class' => AccessControl::class,
+                'rules' => [
+                    [
+                        'allow'   => true,
+                        'actions' => ['profile', 'account', 'networks', 'disconnect', 'delete'],
+                        'roles'   => ['@'],
+                    ],
+                    [
+                        'allow'   => true,
+                        'actions' => ['confirm'],
+                        'roles'   => ['?', '@'],
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @return string|Response
+     * @throws \Yii\base\ExitException
+     * @throws \yii\base\InvalidConfigException
+     */
+    public function actionProfile()
+    {
+        $model = $this->finder->findProfileById(\Yii::$app->user->identity->getId());
+        if ($model == null) {
+            $model = \Yii::createObject(Profile::class);
+            $model->link('user', Yii::$app->user->identity);
+        }
+        $event = $this->getProfileEvent($model);
+        $this->performAjaxValidation($model);
+        $this->trigger(self::EVENT_BEFORE_PROFILE_UPDATE, $event);
+        if ($model->load(\Yii::$app->request->post()) && $model->save()) {
+            \Yii::$app->getSession()->setFlash('success', \Yii::t('user', 'Your profile has been updated'));
+            $this->trigger(self::EVENT_AFTER_PROFILE_UPDATE, $event);
+            return $this->refresh();
+        }
+        return $this->render('profile', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * @return string|Response
+     * @throws Yii\base\ExitException
+     * @throws \yii\base\InvalidConfigException
+     */
+    public function actionAccount()
+    {
+        /** @var SettingsForm $model */
+        $model = Yii::createObject(SettingsForm::class);
+        $event = $this->getFormEvent($model);
+        $this->performAjaxValidation($model);
+        $this->trigger(self::EVENT_BEFORE_ACCOUNT_UPDATE, $event);
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            Yii::$app->session->setFlash('success', Yii::t('user', 'Your account details have been updated'));
+            $this->trigger(self::EVENT_AFTER_ACCOUNT_UPDATE, $event);
+            return $this->refresh();
+        }
+        return $this->render('account', [ 'model' => $model,]);
+    }
+
+    /**
+     * @param int $id
+     * @param string $code
+     *
+     * @return string
+     * @throws \Throwable
+     * @throws \yii\base\InvalidConfigException
+     */
+    public function actionConfirm($id, $code)
+    {
+        $user = $this->finder->findUserById($id);
+        if ($user === null || $this->module->emailChangeStrategy == Module::STRATEGY_INSECURE) {
+            throw new NotFoundHttpException();
+        }
+        $event = $this->getUserEvent($user);
+        $this->trigger(self::EVENT_BEFORE_CONFIRM, $event);
+        $user->attemptEmailChange($code);
+        $this->trigger(self::EVENT_AFTER_CONFIRM, $event);
+        return $this->redirect(['account']);
+    }
+    /**
+     * Displays list of connected network accounts.
+     *
+     * @return string
+     */
+    public function actionNetworks()
+    {
+        return $this->render('networks', ['user' => Yii::$app->user->identity,]);
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return Response
+     * @throws \yii\web\NotFoundHttpException
+     * @throws \yii\web\ForbiddenHttpException
+     * @throws \yii\base\InvalidConfigException
+     */
+    public function actionDisconnect($id)
+    {
+        $account = $this->finder->findAccount()->byId($id)->one();
+        if ($account === null) {
+            throw new NotFoundHttpException();
+        }
+        if ($account->user_id != \Yii::$app->user->id) {
+            throw new ForbiddenHttpException();
+        }
+        $event = $this->getConnectEvent($account, $account->user);
+        $this->trigger(self::EVENT_BEFORE_DISCONNECT, $event);
+        $account->delete();
+        $this->trigger(self::EVENT_AFTER_DISCONNECT, $event);
+        return $this->redirect(['networks']);
+    }
+
+    /**
+     * @return Response
+     * @throws \Exception
+     * @throws \Throwable
+     */
+    public function actionDelete()
+    {
+        if (!$this->module->enableAccountDelete) {
+            throw new NotFoundHttpException(Yii::t('user', 'Not found'));
+        }
+        /** @var User $user */
+        $user  = Yii::$app->user->identity;
+        $event = $this->getUserEvent($user);
+        Yii::$app->user->logout();
+        $this->trigger(self::EVENT_BEFORE_DELETE, $event);
+        $user->delete();
+        $this->trigger(self::EVENT_AFTER_DELETE, $event);
+        Yii::$app->session->setFlash('info', \Yii::t('user', 'Your account has been completely deleted'));
+        return $this->goHome();
+    }
+}
