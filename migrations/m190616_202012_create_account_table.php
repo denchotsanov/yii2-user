5bfc79a61c833829e32e579e2cc5f0cd2067444f
Dencho Tsanov <tsanov.dencho@gmail.com>
2019-06-27 19:01:01 +0300

    fix table name

--- migrations/m190616_202012_create_account_table.php
+++ migrations/m190616_202012_create_account_table.php
@@ -18,13 +18,13 @@ class m190616_202012_create_account_table extends Migration
             'username'   => $this->string()->null(),
 
         ], $this->tableOptions);
-        $this->createIndex('{{%account_unique}}', '{{%account}}', ['provider', 'client_id'], true);
-        $this->addForeignKey('{{%fk_user_account}}', '{{%account}}', 'user_id', '{{%user}}', 'id', $this->cascade, $this->restrict);
+        $this->createIndex('{{%account_unique}}', '{{%social_account}}', ['provider', 'client_id'], true);
+        $this->addForeignKey('{{%fk_user_account}}', '{{%social_account}}', 'user_id', '{{%user}}', 'id', $this->cascade, $this->restrict);
         $this->createIndex('{{%account_unique_code}}', '{{%social_account}}', 'code', true);
     }
     public function down()
     {
-        $this->dropTable('{{%account}}');
+        $this->dropTable('{{%social_account}}');
     }
 
 }
